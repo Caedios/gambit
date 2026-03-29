c1696d47c058fb91a2b05e9e6d1e53fc2ade3911
Caedios <wengweng19971029@gmail.com>
2026-03-29 23:49:52 +0800

    123123

--- app/application/views/user/includes/footer.php
+++
@@ -1,7 +0,0 @@
-      <footer class="sticky-footer">
-          <div class="container my-auto">
-            <div class="copyright text-center my-auto">
-              <span>© 2021 Copyright <a href="https://phpscriptsonline.com" target="_blank">Phpscriptsonline</a></span>
-            </div>
-          </div>
-        </footer>
